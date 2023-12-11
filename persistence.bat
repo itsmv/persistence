b523336b00de9db34f21d8efb0c92e9674078957
itsmv <70952568+itsmv@users.noreply.github.com>
2023-12-11 14:04:04 +0100

    Update persistence.bat

--- persistence.bat
+++ persistence.bat
@@ -2,11 +2,18 @@
 
 REM Get the directory path of the batch script
 set "batchPath=%~dp0"
-$startTime = (Get-Date).AddMinutes(2).ToString("HH:mm")
 
+REM Berechne die Zeit nach 2 Minuten
+for /F "tokens=1-2 delims=:" %%a in ("%time%") do (
+    set /a "hh=1%%a+100, mm=1%%b+2"
+)
 
-REM Construct the full path to the PowerShell script
+REM Stelle sicher, dass führende Nullen für Zeiten unter 10 hinzugefügt werden (z. B. 09)
+if %hh% lss 10 set "hh=0%hh:~-2%"
+if %mm% lss 10 set "mm=0%mm:~-2%"
+
+REM Konstruiere den vollständigen Pfad zum PowerShell-Skript
 set "scriptPath=%batchPath%beep.ps1"
 
-REM Schedule the PowerShell script using schtasks
-schtasks /create /sc HOURLY /tn "MyTasks\BeepTask" /tr "PowerShell.exe -File \"%scriptPath%\"" /st $startTime
+REM Erstelle eine geplante Aufgabe, um das PowerShell-Skript nach 2 Minuten auszuführen
+schtasks /create /sc HOURLY /tn "MyTasks\BeepTask" /tr "PowerShell.exe -File \"%scriptPath%\"" /st %hh%:%mm%
