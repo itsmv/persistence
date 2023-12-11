87173bb2173dbc1a5112c9fcedb48c46bfede0d1
itsmv <70952568+itsmv@users.noreply.github.com>
2023-12-11 13:25:09 +0100

    Update persistence.bat

--- persistence.bat
+++ persistence.bat
@@ -2,9 +2,11 @@
 
 REM Get the directory path of the batch script
 set "batchPath=%~dp0"
+$startTime = (Get-Date).AddMinutes(1).ToString("HH:mm")
+
 
 REM Construct the full path to the PowerShell script
 set "scriptPath=%batchPath%beep.ps1"
 
 REM Schedule the PowerShell script using schtasks
-schtasks /create /sc HOURLY /tn "MyTasks\BeepTask" /tr "PowerShell.exe -File \"%scriptPath%\"" /st %time:~0,2%:%time:~3,2%
+schtasks /create /sc HOURLY /tn "MyTasks\BeepTask" /tr "PowerShell.exe -File \"%scriptPath%\"" /st $startTime
